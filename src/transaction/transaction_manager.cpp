613671bba147b38341197397ca14a82d0d01aeee
songzz <songzhaozhe@126.com>
2019-02-27 02:49:07 -0500

    test

--- src/transaction/transaction_manager.cpp
+++ src/transaction/transaction_manager.cpp
@@ -204,8 +204,7 @@ TransactionQueue TransactionManager::CompletedTransactionsForGC() {
   common::SpinLatch::ScopedSpinLatch guard(&curr_running_txns_latch_);
   TransactionQueue hand_to_gc(std::move(completed_txns_));
   for (auto thread_context : curr_running_workers_) {
-    TransactionQueue tmp(std::move(thread_context->completed_txns_));
-    hand_to_gc.splice_after(hand_to_gc.cbegin(), std::move(tmp));
+    hand_to_gc.splice_after(hand_to_gc.cbegin(), std::move(thread_context->completed_txns_));
   }
   TERRIER_ASSERT(completed_txns_.empty(), "TransactionManager's queue should now be empty.");
   return hand_to_gc;
